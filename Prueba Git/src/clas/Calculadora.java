ecd2da66a313dbdd055a664760f7c0e847900de3
Cruz <03rasputin@gmail.com>
2017-03-22 10:46:36 +0100

    Añadidos Suma, Resta.

--- Prueba Git/src/clas/Calculadora.java
+++ Prueba Git/src/clas/Calculadora.java
@@ -2,9 +2,28 @@ package clas;
 
 public class Calculadora {
 	
+	private int x;
+	private int y;
+	
+	public Calculadora()
+	{
+		
+	}
+	
+	public int suma()
+	{
+		return x + y;
+	}
+	
+	public int resta()
+	{
+		return x - y;
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hola");
+		
 	}
 
 }
